2956659dc9e85125fc413e73cfe7622f98388c64
Jin Qi <jenny_qiqu@hotmail.com>
2023-07-01 12:30:28 -0400

    Completed exercises 1 to 6

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,12 +1,19 @@
 import React from 'react';
 
+import { ToastContext } from '../ToastProvider';
 import Button from '../Button';
+import ToastShelf from '../ToastShelf';
 
 import styles from './ToastPlayground.module.css';
 
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
+  const { addToast } = React.useContext(ToastContext);
+
+  const [message, setMessage] = React.useState('');
+  const [variant, setVariant] = React.useState('notice');
+
   return (
     <div className={styles.wrapper}>
       <header>
@@ -14,7 +21,17 @@ function ToastPlayground() {
         <h1>Toast Playground</h1>
       </header>
 
-      <div className={styles.controlsWrapper}>
+      <ToastShelf />
+
+      <form
+        className={styles.controlsWrapper}
+        onSubmit={(event) => {
+          event.preventDefault();
+          addToast(message, variant);
+          setMessage('');
+          setVariant('notice');
+        }}
+      >
         <div className={styles.row}>
           <label
             htmlFor="message"
@@ -24,38 +41,43 @@ function ToastPlayground() {
             Message
           </label>
           <div className={styles.inputWrapper}>
-            <textarea id="message" className={styles.messageInput} />
+            <textarea
+              id="message"
+              className={styles.messageInput}
+              value={message}
+              onChange={(event) => setMessage(event.target.value)}
+            />
           </div>
         </div>
 
         <div className={styles.row}>
           <div className={styles.label}>Variant</div>
-          <div
-            className={`${styles.inputWrapper} ${styles.radioWrapper}`}
-          >
-            <label htmlFor="variant-notice">
-              <input
-                id="variant-notice"
-                type="radio"
-                name="variant"
-                value="notice"
-              />
-              notice
-            </label>
-
-            {/* TODO Other Variant radio buttons here */}
+          <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
+            {VARIANT_OPTIONS.map((option) => (
+              <label key={option} htmlFor={`variant-${option}`}>
+                <input
+                  id={`variant-${option}`}
+                  type="radio"
+                  name="variant"
+                  value={option}
+                  checked={option === variant}
+                  onChange={(event) => {
+                    setVariant(event.target.value);
+                  }}
+                />
+                {option}
+              </label>
+            ))}
           </div>
         </div>
 
         <div className={styles.row}>
           <div className={styles.label} />
-          <div
-            className={`${styles.inputWrapper} ${styles.radioWrapper}`}
-          >
+          <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
             <Button>Pop Toast!</Button>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
---
+++ src/components/ToastProvider/ToastProvider.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+import useEscapeKey from '../../hooks/use-escape-key.hook';
+
+export const ToastContext = React.createContext();
+
+function ToastProvider({ children }) {
+  const [toasts, setToasts] = React.useState([]);
+
+  const addToast = React.useCallback(
+    (message, variant) => {
+      const newToast = {
+        id: Math.random(),
+        message,
+        variant,
+      };
+
+      setToasts([...toasts, newToast]);
+    },
+    [toasts]
+  );
+
+  const dismissToast = React.useCallback(
+    (id) => {
+      const nextToasts = toasts.filter((element) => {
+        return element.id !== id;
+      });
+
+      setToasts(nextToasts);
+    },
+    [toasts]
+  );
+
+  const clearAllToasts = React.useCallback(() => {
+    setToasts([]);
+  }, []);
+
+  useEscapeKey(clearAllToasts);
+
+  const value = React.useMemo(() => {
+    return { toasts, addToast, dismissToast };
+  }, [toasts, addToast, dismissToast]);
+
+  return (
+    <ToastContext.Provider value={value}>{children}</ToastContext.Provider>
+  );
+}
+
+export default ToastProvider;
